30e1e101f960b21023bb5d096556036d5c8ab8ed
sreeker-reddy <116924654+sreeker-reddy@users.noreply.github.com>
2025-05-15 13:25:33 -0700

    Create Dynamic Pivoting of a Table.sql

---
+++ Dynamic Pivoting of a Table.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE PivotProducts AS
+BEGIN
+	DECLARE @sql_col NVARCHAR(MAX);
+	SELECT @sql_col = ISNULL(@sql_col+',','') + store FROM  Products GROUP BY store ORDER BY store
+	    
+    DECLARE @sql_str NVARCHAR(MAX);
+    SET @sql_str = '
+	SELECT *
+	FROM (SELECT * FROM Products) AS temp 
+	PIVOT (SUM(price) FOR store IN ('+ @sql_col +')) AS pvt 
+	ORDER BY pvt.product_id'
+
+	EXEC (@sql_str)    
+END
